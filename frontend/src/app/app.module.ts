88fb7882a295e4cd4acc0664ca129fb8d21cbdbd
Iago <iaagofelipe@gmail.com>
2020-10-14 16:50:35 -0300

    created an link with front and database, waiting a form creation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,18 +7,33 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HeaderComponent} from './components/template/header/header.component';
 
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component';
+import {FooterComponent} from './components/template/footer/footer.component';
+import {NavComponent} from './components/template/nav/nav.component';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import {HomeComponent} from './views/home/home.component';
+import {ProductCrudComponent} from './views/product-crud/product-crud.component';
+import {RedDirective} from './directives/red.directive';
+import {ForDirective} from './directives/for.directive';
+import {ProductCreateComponent} from './components/product/product-create/product-create.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent,
-        NavComponent
+        NavComponent,
+        HomeComponent,
+        ProductCrudComponent,
+        RedDirective,
+        ForDirective,
+        ProductCreateComponent
     ],
     imports: [
         BrowserModule,
@@ -27,6 +42,10 @@ import {MatListModule} from '@angular/material/list';
         MatToolbarModule,
         MatSidenavModule,
         MatListModule,
+        MatCardModule,
+        MatButtonModule,
+        MatSnackBarModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
